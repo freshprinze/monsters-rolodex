15d7833a7376c92464bf2fafdadc193a06c17e2f
Asiri Warnakulasuriya <asiriw2002@gmail.com>
2020-01-03 09:36:27 +0530

    Updates

---
+++ precache-manifest.b8886deabae6bf0291f5c7e971bc6ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f9d1d058481573cfc259eae161c067",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9cbdfbf54b1145de8da8",
+    "url": "/monsters-rolodex/static/css/main.55a013db.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9cbdfbf54b1145de8da8",
+    "url": "/monsters-rolodex/static/js/main.d3f0fa07.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
